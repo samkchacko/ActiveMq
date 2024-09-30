a46256843d05bca24317ab2663030b883c79352f
Sam K Chacko <chackoksam1@gmail.com>
2024-09-30 09:45:00 +0530

    first commit

---
+++ Speridian.ActiveMQDebug.Consumer/Speridian.ActiveMQDebug.Consumer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace Speridian.ActiveMQDebug.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string brokerUri = "tcp://127.0.0.1:61621";//?jms.prefetchPolicy.queuePrefetch=1
+            //"tcp://127.0.0.1:61621";//External
+            
+            string queueName = "OfficerSched.Hearings.PD200";
+                //"OfficerSched.Response.PD200.APD";
+                //"OfficerSched.Request";
+            try {
+                IConnectionFactory factory = new ConnectionFactory(brokerUri);
+                Console.WriteLine("Factory Set");
+                using (IConnection connection = factory.CreateConnection())
+                {
+                    Console.WriteLine("Connection going to start");
+                    connection.Start();
+                    Console.WriteLine("Connection is started");
+                    using(ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge))//AcknowledgementMode.AutoAcknowledge
+                    {
+                        Console.WriteLine("Session created");
+                        IDestination destination = session.GetQueue(queueName);
+                        Console.WriteLine("Queue set");
+                        using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                        {
+                            Console.WriteLine("Consumer created");
+                            IMessage message=consumer.Receive(TimeSpan.FromSeconds(100));
+                            Console.WriteLine("message recived");
+                            if(message is ITextMessage textMessage)
+                            {
+                                Console.WriteLine("Received message with text: " + textMessage.Text);
+                            }
+                            else
+                            {
+                                Console.WriteLine("No message received within the time limit.");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Speridian.ActiveMQDebugApp/ActiveMQDebugApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Serilog;
+using Serilog.Core;
+
+
+namespace ActiveMQDebugApp
+{
+    internal class Program
+    {
+        //private static readonly ILogger logger = new LoggerConfiguration().WriteTo.File("C:/Users/sam.chacko/Desktop/Sooraj/Speridian.ActiveMQDebugApp/Log/Debuglog.txt").CreateLogger();
+        static void Main(string[] args)
+        {
+            string brokerUri = "tcp://127.0.0.1:61621";
+               
+            try
+            {
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                IConnectionFactory factory = new ConnectionFactory(brokerUri);
+                using (IConnection connection = factory.CreateConnection())
+                {
+                    connection.Start();
+                    using (ISession session = connection.CreateSession(AcknowledgementMode.AutoAcknowledge))
+                    {
+                        IDestination destination = session.GetQueue("DebugQueue");
+                        using (IMessageProducer producer = session.CreateProducer(destination))
+                        {
+                            ITextMessage textMessage = producer.CreateTextMessage("Helllo Sam");
+                            producer.Send(textMessage);
+                            Console.WriteLine("Message Sent");
+                            //logger.Information("\nMessage Sent");
+                            //logger.Information("\n===============================================\n");
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                //logger.Error(e.ToString());
+                //logger.Error("\n===============================================\n");
+            }
+        }
+    }
+}
